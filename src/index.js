a436cb678658b09ded7e5f3013e7846c949563ab
Alexander Riccio <test35965+learn@gmail.com>
2019-07-16 15:41:13 -0400

    deletes tasks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.querySelector("#create-task-form");
+  const tasks = document.querySelector("#tasks");
   taskForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const userInputField = document.querySelector("#new-task-description").value;
-    const userInputString = userInputField;
-    const tasks = document.querySelector("#tasks");
+    const userInputString = document.querySelector("#new-task-description").value;
     const newTask = document.createElement("li");
     newTask.textContent = userInputString;
     tasks.appendChild(newTask);
-    if (event.target.value === "Create New Task") {
-    }
-
+    const newDeleteButton = document.createElement("button")
+    newDeleteButton.innerHTML = "delete"
+    newDeleteButton.addEventListener('click', deleteButtonClick);
+    newTask.appendChild(newDeleteButton);
+    
   });
+
+  //tasks.addEventListener('click', deleteButtonClick);
 });
+
+
+function deleteButtonClick(event) {
+  if (event.target.type === "submit") {
+    this.parentElement.remove()
+  }
+}
