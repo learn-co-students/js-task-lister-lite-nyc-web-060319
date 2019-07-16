58fa91c031fecc5f010cd277a913c24b7dda0145
Alexander Riccio <test35965+learn@gmail.com>
2019-07-16 16:11:17 -0400

    priority

--- src/index.js
+++ src/index.js
@@ -2,19 +2,36 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.querySelector("#create-task-form");
   const tasks = document.querySelector("#tasks");
-  taskForm.addEventListener('submit', submitTaskEventHandler;
+  taskForm.addEventListener('submit', submitTaskEventHandler);
 });
+
+function taskStyleFromColor(priorityString) {
+  switch(priorityString) {
+    case "high":
+      return "red";
+    case "medium":
+      return "yellow";
+    case "low":
+      return "green";
+    default:
+      console.error("Bad priority: " + priorityString)
+  }
+}
 
 function createNewTask() {
   const userInputString = document.querySelector("#new-task-description").value;
+  const userInputPriority = document.getElementById("new-task-input").value;
   const newTask = document.createElement("li");
   newTask.textContent = userInputString;
+  newTask.style.color = taskStyleFromColor(userInputPriority)
+  return newTask
 }
 
 function createDeleteButton() {
   const newDeleteButton = document.createElement("button")
   newDeleteButton.innerHTML = "delete"
   newDeleteButton.addEventListener('click', deleteButtonClick);
+  return newDeleteButton;
 }
 
 function submitTaskEventHandler(event) {
