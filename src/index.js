4df31ca8ca77cc4028985314aa5d34dae2d6ff84
eschechter <schechter.evan@gmail.com>
2019-07-16 15:31:18 -0400

    basic functionality and delete button with js array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const listEles = []
+
+  function submitHandler(event) {
+    const input = event.target.querySelector('#new-task-description').value;
+    const toAddTo = document.querySelector('ul#tasks')
+    const eleToAdd = document.createElement('li')
+    listEles.push(eleToAdd)
+    eleToAdd.innerText = input;
+    toAddTo.appendChild(eleToAdd)
+  }
+  function deleteHandler(event){
+    toDelete = listEles.pop()
+    toDelete.remove()
+  }
+  document.querySelector('#create-task-form').addEventListener(
+    'submit', function(e){
+      e.preventDefault();
+      
+      submitHandler(e)
+    })
+
+    document.querySelector('#delete-button').addEventListener(
+      'click', function(e){
+        deleteHandler(e)
+        e.preventDefault()
+      }
+    )
 });
