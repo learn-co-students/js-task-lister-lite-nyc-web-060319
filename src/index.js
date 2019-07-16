41d322821c57eaf9f56fb047df17b1f25b8e7126
Alexander Riccio <test35965+learn@gmail.com>
2019-07-16 15:25:55 -0400

    adds task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userInputField = document.querySelector("#new-task-description").value;
+    const userInputString = userInputField;
+    const tasks = document.querySelector("#tasks");
+    const newTask = document.createElement("li");
+    newTask.textContent = userInputString;
+    tasks.appendChild(newTask);
+    if (event.target.value === "Create New Task") {
+    }
+
+  });
 });
