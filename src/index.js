33a839c56cc25f7b1be41793f1e75f609a9c65f4
CindyChenx <chen_cx0128@hotmail.com>
2019-07-16 17:09:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function myFunction(){
+    let newTask = document.querySelector('#new-task-description');
+    let taskString = newTask.value;
+
+    let unorderedList = document.querySelector('#tasks');
+    let newList = document.createElement('li');
+    newList.innerText = taskString;
+    unorderedList.appendChild(newList);
+  }
+
+  
+  document.querySelector('#create-task-form').addEventListener('submit', (eventObj) => {
+    console.log(eventObj);
+    eventObj.preventDefault();
+    myFunction();
+    console.log(newTask.value);
+  });
+
+  let deleteButton = document.createElement('button');
+  deleteButton.innerText = "DELETE";
+
+  document.body.appendChild(deleteButton);
+
+  deleteButton.addEventListener('click', () => {
+    let unorderList = document.querySelector('#tasks');
+    unorderList.lastElementChild.remove(); //lastChild
+  });
+
+
 });
+
+
