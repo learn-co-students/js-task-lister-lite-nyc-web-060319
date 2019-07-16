bb8cb72f5edaf50c3b97ef4659658a5f5114ef7b
Alexander Riccio <test35965+learn@gmail.com>
2019-07-16 15:46:29 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const taskForm = document.querySelector("#create-task-form");
   const tasks = document.querySelector("#tasks");
-  taskForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const userInputString = document.querySelector("#new-task-description").value;
-    const newTask = document.createElement("li");
-    newTask.textContent = userInputString;
-    tasks.appendChild(newTask);
-    const newDeleteButton = document.createElement("button")
-    newDeleteButton.innerHTML = "delete"
-    newDeleteButton.addEventListener('click', deleteButtonClick);
-    newTask.appendChild(newDeleteButton);
-    
-  });
-
-  //tasks.addEventListener('click', deleteButtonClick);
+  taskForm.addEventListener('submit', submitTaskEventHandler;
 });
 
+function createNewTask() {
+  const userInputString = document.querySelector("#new-task-description").value;
+  const newTask = document.createElement("li");
+  newTask.textContent = userInputString;
+}
+
+function createDeleteButton() {
+  const newDeleteButton = document.createElement("button")
+  newDeleteButton.innerHTML = "delete"
+  newDeleteButton.addEventListener('click', deleteButtonClick);
+}
+
+function submitTaskEventHandler(event) {
+  event.preventDefault();
+  const newTask = createNewTask();
+  tasks.appendChild(newTask);
+  const newDeleteButton = createDeleteButton();
+  newTask.appendChild(newDeleteButton);
+}
 
 function deleteButtonClick(event) {
   if (event.target.type === "submit") {
