b864a49eb1fbe43947b89ce5f1e9ea1adf6a79bb
eschechter <schechter.evan@gmail.com>
2019-07-16 15:52:02 -0400

    Stretch goal achievement UNLOCKED

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const listEles = []
 
   function submitHandler(event) {
+    debugger
+    const priority = event.target.querySelector('#outer').value;
+
     const input = event.target.querySelector('#new-task-description').value;
     const toAddTo = document.querySelector('ul#tasks')
     const eleToAdd = document.createElement('li')
-    listEles.push(eleToAdd)
     eleToAdd.innerText = input;
+
+    switch (priority) {
+      case "High Priority":
+        eleToAdd.style = "color: red;"
+        break;
+    
+      case "Medium Priority":
+        eleToAdd.style = "color: yellow;"
+
+        break;
+      case "Low Priority":
+        eleToAdd.style = "color: green;"
+        break;
+    }
+
+    
     toAddTo.appendChild(eleToAdd)
   }
   function deleteHandler(event){
-    toDelete = listEles.pop()
+    const toDelete = document.querySelector('ul#tasks').lastChild
     toDelete.remove()
   }
   document.querySelector('#create-task-form').addEventListener(
     'submit', function(e){
       e.preventDefault();
-      
       submitHandler(e)
     })
 
